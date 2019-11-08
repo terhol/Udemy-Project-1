e8b904fb7367481adcbebde7288dad0885b700f5
Holm <tereza.holm@sogeti.com>
2019-11-08 15:31:19 +0100

    Exception Handler added

---
+++ src/main/java/ExceptionHandler.java
@@ -0,0 +1,15 @@
+import java.sql.SQLException;
+
+public class ExceptionHandler {
+
+    public static void handleException(Exception e){
+        if(e instanceof SQLException){
+            SQLException sqlException = (SQLException) e;
+            System.out.println("Error code: " + sqlException.getErrorCode());
+            System.out.println("SQL state: " + sqlException.getSQLState());
+        }
+        System.out.println("SQL message: " + e.getMessage());
+        System.out.println("Stack trace: ");
+        e.printStackTrace();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         }
         catch(Exception e){
-            e.printStackTrace();
+            ExceptionHandler.handleException(e);
         }
 
     }
